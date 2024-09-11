40dccd1c1fcab0b5b81353ecdaf0e6bca8b5a132
deajjsrt <yonathandea0@gmail.com>
2024-09-11 17:45:19 +0700

    Module Product

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//route resources for products
+Route::resource('/products', \App\Http\Controllers\ProductController::class);
